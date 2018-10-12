b945c4d0adff28829df863523075cc2bd21a10f6
Andrew Paterson <andrew@hyraxbio.co.za>
2018-10-12 13:48:24 +0200

    Fixed paths to export components.

--- app/components/power-datetime-picker.js
+++ app/components/power-datetime-picker.js
@@ -1,1 +1,1 @@
-export { default } from 'ember-extended-fields/power-datetime-picker';
+export { default } from 'ember-extended-fields/components/power-datetime-picker';
--- app/components/tag-selector.js
+++ app/components/tag-selector.js
@@ -1,1 +1,1 @@
-export { default } from 'ember-extended-fields/tag-selector';
+export { default } from 'ember-extended-fields/components/tag-selector';
