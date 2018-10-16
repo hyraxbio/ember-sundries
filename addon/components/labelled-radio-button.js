2f779411d12a902856e33ec8a335ae8b8190c778
Andrew Paterson <andrew@hyraxbio.co.za>
2018-10-16 15:02:01 +0200

    Added labelled radio button and checkbox components.

---
+++ addon/components/labelled-radio-button.js
@@ -0,0 +1,9 @@
+import Component from '@ember/component';
+import layout from '../templates/components/labelled-radio-button';
+
+export default Component.extend({
+  layout,
+  tagName: "div",
+  classNames: ["labelled-radio-button"],
+  classNameBindings: ["disabled:disabled"],
+});
