3d5aa25fa3a782c0c6d9c5c16aad21ac56bfd615
Andrew Paterson <andrew@hyraxbio.co.za>
2018-12-17 17:00:20 +0200

    Various small updates.

--- addon/components/ember-extended-elements/in-page-alert.js
+++ addon/components/ember-extended-elements/in-page-alert.js
@@ -1,8 +1,17 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import layout from '../../templates/components/ember-extended-elements/in-page-alert';
 
 export default Component.extend({
   layout,
   classNames: ['in-page-alert'],
-  classNameBindings: ['type', 'classes'],
+  classNameBindings: ['type', 'classes', 'textClass'],
+
+  textClass: computed('type', function() {
+    return `text-${this.get('type')}`;
+  }),
+
+  fillClass: computed('type', function() {
+    return `fill-${this.get('type')}`;
+  }),
 });
