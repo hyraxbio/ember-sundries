ff51bd26adcfea2f06e16cb62baf2a4eb7636e8c
Andrew Paterson <andrew@hyraxbio.co.za>
2019-01-10 13:57:57 +0200

    Added string-contains helper.

---
+++ addon/helpers/ember-extended-elements/string-contains.js
@@ -0,0 +1,9 @@
+import { helper } from '@ember/component/helper';
+
+export function stringContains(params/*, hash*/) {
+  var testString = params[0].toLowerCase();
+  var searchString = params[1].toLowerCase();
+  return testString.indexOf(searchString) > -1;
+}
+
+export default helper(stringContains);
---
+++ app/helpers/ember-extended-elements/string-contains.js
@@ -0,0 +1,1 @@
+export { default, emberExtendedElementsStringContains } from 'ember-extended-fields/helpers/ember-extended-elements/string-contains';
