78b5de5800c703de0d196f88ca4efcf818f17288
Andrew Paterson <andrew@hyraxbio.co.za>
2019-02-12 16:15:41 +0200

    Added loading button and updated confirm cancel popout.

---
+++ addon/components/ember-extended-elements/loading-button.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+import layout from '../../templates/components/ember-extended-elements/loading-button';
+
+export default Component.extend({
+  layout,
+  tagName: ''
+});
