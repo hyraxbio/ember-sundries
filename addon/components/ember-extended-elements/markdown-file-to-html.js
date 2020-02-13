b34e8836d74e398765c1eb8c54c44d17293cd5cb
Andrew Paterson <andrew@hyraxbio.co.za>
2020-02-13 11:19:50 +0200

    Small bugfixes.

--- addon/components/ember-extended-elements/markdown-file-to-html.js
+++ addon/components/ember-extended-elements/markdown-file-to-html.js
@@ -1,8 +1,10 @@
 import Component from '@ember/component';
 import layout from '../../templates/components/ember-extended-elements/markdown-file-to-html';
-import fetch from 'fetch';
+
 export default Component.extend({
   layout,
+  attributeBindings: ['dataTestComponent:data-test-component'],
+  dataTestComponent: 'markdown-content',
   
   didInsertElement() {
     fetch(this.get('filePath')).then(response => {
--- addon/helpers/ember-extended-elements/array-join.js
+++ addon/helpers/ember-extended-elements/array-join.js
@@ -1,9 +1,12 @@
 import { helper } from '@ember/component/helper';
 
 export function arrayJoin(params) {
-var array = params[0];
-var joinWithString = params[1] || ', ';
-  return array.join(joinWithString);
+  var joinWith = params[1] || ', ';
+  if (!params[0]) { return; }
+  if (!Array.isArray(params[0])) { 
+    console.warn(`[ember-extended-elements/array-join] You must pass an array as the first argument, You passed ${params[0]}] `);
+    return params[0]; 
+  }
+  return params[0].join(joinWith);
 }
-
 export default helper(arrayJoin);
