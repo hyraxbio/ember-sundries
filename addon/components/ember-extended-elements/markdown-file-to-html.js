5ec37f3c474ddd74aa3481fc9f0e5611c9a966b6
andrew-paterson <andrew.paterson9@gmail.com>
2020-09-17 14:01:48 +0200

    Small updates.

--- addon/components/ember-extended-elements/markdown-file-to-html.js
+++ addon/components/ember-extended-elements/markdown-file-to-html.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import layout from '../../templates/components/ember-extended-elements/markdown-file-to-html';
+import fetch from 'fetch';
 
 export default Component.extend({
   layout,
