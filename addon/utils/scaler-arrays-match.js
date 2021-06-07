e74df246a3543a2d9d5f98bcb17e4b7f4f2fc058
andrew-paterson <andrew.paterson9@gmail.com>
2021-06-07 15:49:14 +0200

    Styles.

--- addon/utils/scaler-arrays-match.js
+++ addon/utils/scaler-arrays-match.js
@@ -1,3 +1,6 @@
-export default function scalerArraysMatch(array1, array2) {
+export default function scalerArraysMatch(array1, array2, options = {}) {
+  if (options.matchOrder) {
+    return array1.length === array2.length && array1.every(function(value, index) { return value === array2[index];});
+   }
   return array1.length === array2.length && array1.sort().every(function(value, index) { return value === array2.sort()[index];});
 }
