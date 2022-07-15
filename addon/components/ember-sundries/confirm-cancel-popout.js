b114346f6c3ae1328538f0cbc52924da5ea1e84b
Andrew Paterson <andrew.paterson9@gmail.com>
2022-07-15 10:53:36 +0100

    Add data-test-id to confirm cancel popout.

--- addon/components/ember-sundries/confirm-cancel-popout.js
+++ addon/components/ember-sundries/confirm-cancel-popout.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import layout from '../../templates/components/ember-sundries/confirm-cancel-popout';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   layout,
@@ -7,6 +8,10 @@ export default Component.extend({
   classNames: ['confirm-cancel-container'],
   classNameBindings: ['containerClasses', 'colorScheme'],
   'data-test-class': 'confirm-cancel-dialogue',
+  // attributeBindings: ['dataTest']
+  'data-test-name': computed('mainButtonText', function() {
+    return (this.mainButtonText || '').replace(/\s+/g, '-').toLowerCase()
+  }),
 
   didInsertElement() {
     if (this.get('positionStatic')) {
