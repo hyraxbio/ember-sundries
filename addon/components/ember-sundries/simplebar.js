435e3702c993385765487883016fb0d73d0682b7
Andrew Paterson <andrew.paterson9@gmail.com>
2023-03-20 15:48:56 +0200

    Various

--- addon/components/ember-sundries/simplebar.js
+++ addon/components/ember-sundries/simplebar.js
@@ -1,6 +1,17 @@
 import SimpleBar from 'ember-simplebar/components/simple-bar';
 import layout from '../../templates/components/ember-sundries/simplebar';
+import { computed } from '@ember/object';
 
 export default SimpleBar.extend({
-  layout
+  layout,
+  attributeBindings: ['style:style'],
+
+  didReceiveAttrs() {
+    if (!this.element) { return; }
+    this.set('contentWidth', this.element.querySelector('.ember-simplebar-dummy').offsetWidth);
+  },
+
+  style: computed('contentWidth', function() {
+    return `width: ${this.contentWidth}px`.htmlSafe();
+  }),
 })
--- addon/utils/safe-name.js
+++ addon/utils/safe-name.js
@@ -1,4 +1,4 @@
-export default function sanitiseClassname(className) {
+export default function safeName(className) {
   var reg = /^\d+$/;
   var className = className.replace(/\s/g, '-').replace(/-+/g, '-').replace(/[^0-9a-zA-Z_-]/g, '').toLowerCase();
   var firstChar = className.split('')[0]; 
