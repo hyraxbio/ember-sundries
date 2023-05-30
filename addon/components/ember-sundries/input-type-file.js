46ec5811ea892b713f646b99ccb6825a45e92d88
Andrew Paterson <andrew.paterson9@gmail.com>
2023-05-30 14:40:07 +0200

    Don't export TextField component.

--- addon/components/ember-sundries/input-type-file.js
+++ addon/components/ember-sundries/input-type-file.js
@@ -1,22 +1,21 @@
 import { isEmpty } from '@ember/utils';
 import layout from '../../templates/components/ember-sundries/input-type-file';
-import TextField from '@ember/component/text-field';
+import Component from '@ember/component';
 
-export default TextField.extend({
+export default Component.extend({
   layout,
-  type: 'file',
-
-  change: function(e) {
-    var input = e.target;
-    if (!isEmpty(input.files)) {
-      this.send('sendFiles', input.files);
-      e.target.closest('.file-select-button').reset();
-    }
-  },
 
   actions: {
-    sendFiles: function(files) {
+    wasChanged(e) {
+      var input = e.target;
+      if (!isEmpty(input.files)) {
+        this.send('sendFiles', input.files);
+        e.target.closest('.file-select-button').reset();
+      }
+    },
+
+    sendFiles: function (files) {
       this.sendFiles(files);
     },
-  }
+  },
 });
