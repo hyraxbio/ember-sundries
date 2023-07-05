857c1b92f3d7ebd603a24ba32f618bc4b1f38d2a
Andrew Paterson <andrew.paterson9@gmail.com>
2023-07-05 13:51:03 +0200

    Update modals

--- addon/components/ember-sundries/confirm-cancel-modal.js
+++ addon/components/ember-sundries/confirm-cancel-modal.js
@@ -2,5 +2,13 @@ import Component from '@ember/component';
 import layout from '../../templates/components/ember-sundries/confirm-cancel-modal';
 
 export default Component.extend({
-  layout
+  layout,
+  actions: {
+    closeModal() {
+      const activeModalProp = this.activeModal
+        ? 'activeModal'
+        : 'modalDialogs.activeModal'; /// Use the prop passed in or fallback to the activeModal prop on tthe modalDialogs service
+      this.set(activeModalProp, false);
+    },
+  },
 });
