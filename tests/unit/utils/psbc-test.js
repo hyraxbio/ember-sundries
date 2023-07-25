a41896952677ed96460ee4da77cb0501f62c7a33
Andrew Paterson <andrew.paterson9@gmail.com>
2023-07-25 12:41:31 +0200

    Add pSBC util

---
+++ tests/unit/utils/psbc-test.js
@@ -0,0 +1,11 @@
+import psbc from 'dummy/utils/psbc';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | psbc', function() {
+
+  // TODO: Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = psbc();
+    assert.ok(result);
+  });
+});
