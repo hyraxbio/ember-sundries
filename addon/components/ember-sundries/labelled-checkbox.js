d9db9fd3bef6f47ddcd86357a42d56056760565b
Andrew Paterson <andrew.paterson9@gmail.com>
2024-08-01 15:50:02 +0200

    Update labelled checkbox with better id and accessibility handling

--- addon/components/ember-sundries/labelled-checkbox.js
+++ addon/components/ember-sundries/labelled-checkbox.js
@@ -1,7 +1,18 @@
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
+import { guidFor } from '@ember/object/internals';
 
 export default class LabelledCheckbox extends Component {
+  _uid = guidFor(this);
+
+  get commonId() {
+    return this.args.elementId || this._uid;
+  }
+
+  get hasLabel() {
+    return this.args.label || this.args.labelComponent;
+  }
+
   @action
   checkboxClicked(event) {
     if (this.args.changedAction) {
